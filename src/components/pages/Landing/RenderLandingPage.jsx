c2b76c3220d9970e87cbb20a0a9d5123e316a019
petertrai <petertrai@gmail.com>
2023-07-20 13:32:13 -0700

    Add styled graphs and statistics to landing page

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 // for the purposes of testing PageNav
-// import PageNav from '../../common/PageNav';
+import PageNav from '../../common/PageNav';
 
 function RenderLandingPage(props) {
   const scrollToTop = () => {
@@ -32,7 +32,35 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      <div className="graphs-section">
+        <div className="graph">
+          <img
+            src={GrantRatesByOfficeImg}
+            alt="Grant Rates By Office Graph"
+            className="graph-img"
+          />
+          <p>Search Grant Rates By Office</p>
+        </div>
+
+        <div className="graph">
+          <img
+            src={GrantRatesByNationalityImg}
+            alt="Grant Rates by Nationality Graph"
+            className="graph-img"
+          />
+          <p>Search Grant Rates By Nationality</p>
+        </div>
+
+        <div className="graph">
+          <img
+            src={GrantRatesOverTimeImg}
+            alt="Grant Rates over Time Graph"
+            className="graph-img"
+          />
+          <p>Search Grant Rates Over Time</p>
+        </div>
+      </div>
+
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -61,7 +89,38 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+
+        <div className="bottom-section">
+          <div>
+            <h1 id="stat-main-title">Systemic Disparity Insights</h1>
+          </div>
+          <div className="stat-container">
+            <div className="stat">
+              <h1>36%</h1>
+              <p>
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </p>
+            </div>
+            <div className="stat">
+              <h1>5%</h1>
+              <p>
+                The New York asylum grant rate dropped to 5 percent in fiscal
+                year 2020.
+              </p>
+            </div>
+            <div className="stat">
+              <h1>6x Lower</h1>
+              <p>
+                Between fiscal year 2017 and 2020, the New York asylum office's
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </p>
+            </div>
+          </div>
+        </div>
+
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
